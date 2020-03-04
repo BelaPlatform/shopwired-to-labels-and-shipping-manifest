8e54a48e999754b9df3ca8c7b93fa9028507a185
Giulio Moro <giuliomoro@yahoo.it>
2020-03-04 13:03:12 +0000

    Printing appropriate usage and more details on generated files

--- build_orders.sh
+++ build_orders.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -e
+PDF=addres_label_14_a4.pdf
+
+[ -z "$1" ] && { echo "Specify input file: \``basename $0` <filename.csv>'"; exit 1; }
+rm -rf $PDF labels_tex/$PDF
 python bela_order_organizer.py -f $1
 cd labels_tex
-pdflatex addres_label_14_a4 > /dev/null
-mv addres_label_14_a4.pdf ../
+pdflatex addres_label_14_a4 > /dev/null || { echo "Error while building the pdf"; exit 1; }
+mv $PDF ../
+printf "1 pdf file was created:\n\t$PDF containing address labels\n"

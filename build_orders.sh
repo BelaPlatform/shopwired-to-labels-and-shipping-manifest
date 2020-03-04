baf3571a910d5b7d32bc0e16a5a41238e681b568
Giulio Moro <giuliomoro@yahoo.it>
2020-03-04 13:08:07 +0000

    Cleaning output files before starting, generating zip archive

--- build_orders.sh
+++ build_orders.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 PDF=addres_label_14_a4.pdf
+ZIP=orders-labels-manifest.zip
 
 [ -z "$1" ] && { echo "Specify input file: \``basename $0` <filename.csv>'"; exit 1; }
-rm -rf $PDF labels_tex/$PDF
+OUTPUTS="$PDF items_to_ship.txt order_info.txt MANIFEST.csv NEW_ORDER.csv"
+rm -rf labels_tex/$PDF $OUTPUTS $ZIP
 python bela_order_organizer.py -f $1
 cd labels_tex
 pdflatex addres_label_14_a4 > /dev/null || { echo "Error while building the pdf"; exit 1; }
 mv $PDF ../
+cd ..
 printf "1 pdf file was created:\n\t$PDF containing address labels\n"
+zip -rq $ZIP $OUTPUTS
+printf "1 zip file was created:\n\t$ZIP containing all the above\n"
